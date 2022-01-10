63d10895db5c60fac8451f9959e56c59a80ca3fa
Vlad Tkachenko <vladtka@ukr.net>
2022-01-10 12:27:53 +0200

    add new route

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,36 @@ const routes = [
     meta: { layout: "main" },
     component: () => import("@/views/404Error.vue"),
   },
+  {
+    path: "/login",
+    name: "login",
+    meta: { layout: "empty" },
+    component: () => import("@/views/login.vue"),
+  },
+  {
+    path: "/signup",
+    name: "signUp",
+    meta: { layout: "empty" },
+    component: () => import("@/views/signup.vue"),
+  },
+  {
+    path: "/signupNext",
+    name: "signUpNext",
+    meta: { layout: "empty" },
+    component: () => import("@/views/signupNext.vue"),
+  },
+  {
+    path: "/forgot",
+    name: "forgot",
+    meta: { layout: "empty" },
+    component: () => import("@/views/forgot.vue"),
+  },
+  {
+    path: "/verify",
+    name: "verify",
+    meta: { layout: "empty" },
+    component: () => import("@/views/verify.vue"),
+  },
 ];
 
 const router = createRouter({
