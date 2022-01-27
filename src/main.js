625f86166f07dacc2d7256aaed7f4c8c7948197d
Vlad Tkachenko <vladtka@ukr.net>
2022-01-27 16:21:20 +0200

    add toast

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import Toaster from "@meforma/vue-toaster";
 
 import vueCountryRegionSelect from "vue3-country-region-select";
 
-createApp(App).use(store).use(router).use(vueCountryRegionSelect).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(Toaster, {
+    position: "top-right",
+  })
+  .use(vueCountryRegionSelect)
+  .mount("#app");
