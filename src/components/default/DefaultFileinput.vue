4c890087ce21644f554c7741322ed35c04500f3e
Vlad Tkachenko <vladtka@ukr.net>
2022-02-09 16:15:32 +0200

    fixed

--- src/components/default/DefaultFileinput.vue
+++ src/components/default/DefaultFileinput.vue
@@ -14,8 +14,8 @@
     <div class="file-input__preview" v-if="!disablePreview">
       <img
         :src="
-          img ||
           fileurl ||
+          img ||
           'https://solovero.com/assets/camaleon_cms/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef.png'
         "
         alt=""
